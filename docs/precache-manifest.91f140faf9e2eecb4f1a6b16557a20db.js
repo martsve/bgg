4f7f9146692bc4a651da4c83f75e4260f76e64ed
martsve <martsve@gmail.com>
2022-05-12 22:23:57 +0200

    add docs

---
+++ docs/precache-manifest.91f140faf9e2eecb4f1a6b16557a20db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bb4b2aceffcab349c91d1f5ca32043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0d1eed953b7ca12050a",
+    "url": "/static/css/main.c000ec7b.chunk.css"
+  },
+  {
+    "revision": "58ef48bf59c0a15317ed",
+    "url": "/static/js/2.b39e198d.chunk.js"
+  },
+  {
+    "revision": "e0d1eed953b7ca12050a",
+    "url": "/static/js/main.6db2f8bb.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
