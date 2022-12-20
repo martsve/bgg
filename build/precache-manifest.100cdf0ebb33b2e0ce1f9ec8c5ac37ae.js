683bdca09fc0425006719c1d245f9bf53958f38f
martsve <martsve@gmail.com>
2022-12-20 10:56:25 +0100

    build to build dir

---
+++ build/precache-manifest.100cdf0ebb33b2e0ce1f9ec8c5ac37ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0acc29f884a3b87848338951e08d47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d14a6083cdf00c7a548f",
+    "url": "./static/css/main.c000ec7b.chunk.css"
+  },
+  {
+    "revision": "8f9cc3dc8092dd05bb76",
+    "url": "./static/js/2.ee0f9c57.chunk.js"
+  },
+  {
+    "revision": "d14a6083cdf00c7a548f",
+    "url": "./static/js/main.f408fa77.chunk.js"
+  },
+  {
+    "revision": "2db0ae1ceb9b7d99ef35",
+    "url": "./static/js/runtime-main.99539369.js"
+  }
+]);
